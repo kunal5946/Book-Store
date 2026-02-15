c1b554f4ec90beebca949056253bc97375931f91
kunal5946 <kunalsripalanchu@gmail.com>
2026-02-15 12:00:30 +0530

    updated index.js to make project ready for deployment

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,29 @@
-import 'dotenv/config'
-import express from "express"
-import mongoose from "mongoose"
-import bookRoute from "./route/book.route.js"
-import cors from "cors"
-import userroute from "./route/user.route.js"
+import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
+import bookRoute from "./route/book.route.js";
+import cors from "cors";
+import userroute from "./route/user.route.js";
 
-const app = express()
-app.use(cors())
+const app = express();
 
+app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-const PORT = process.env.PORT || 4001
-const URI = process.env.mongoDBURI
 
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MONGO_URI;
 
+// connect mongo
+mongoose
+  .connect(URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("Mongo error:", err));
 
-//connect to mongodb
-mongoose.connect(URI).then(() => console.log("mongo DB connected")).catch(err => console.log("error is :", err))
-
-
-//defining routes
-app.use("/book", bookRoute)
-app.use("/users", userroute)
-
+// routes
+app.use("/book", bookRoute);
+app.use("/users", userroute);
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
